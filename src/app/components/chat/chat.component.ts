15222b37577f6994e8cba5666e13b42807e2c99e
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-06 17:24:09 -0600

    Scroll

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -12,19 +12,29 @@ import { Subscription } from 'rxjs';
 export class ChatComponent implements OnInit, OnDestroy {
   texto=''
   mensajeSubscription:Subscription;
+  elemento:HTMLElement;
+  mensajes:any[]=[];
   constructor(
     public chatService:ChatService
   ) { }
 
   ngOnInit() {
 
+    this.elemento=document.getElementById('chat-mensajes')
     this.mensajeSubscription=this.chatService.getMensaje().subscribe(msg=>{
-      console.log(msg);
       
+
+      this.mensajes.push(msg)
+      setTimeout(()=>{
+        this.elemento.scrollTop=this.elemento.scrollHeight;
+      },50);
     })
   }
 
   enviar(){
+    if(this.texto.trim().length===0){
+      return
+    }
     console.log(this.texto);
     this.chatService.sendMensaje(this.texto)
     this.texto=''
