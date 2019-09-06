b624c2a133f9b68e09449f459d85f174bf9de40d
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-06 16:07:14 -0600

    primer commit

---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(
+    public wsService:WebsocketService
+  ) { }
+
+
+  sendMensaje(mensaje:string){
+
+    const payload ={
+      de:'Julio',
+      cuerpo:mensaje
+    }
+
+    this.wsService.emit('mensaje', payload)
+
+  }
+}
